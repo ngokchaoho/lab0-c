c0821c51fc6b1fcad4e4a2f19f4ed43e2e337fa6
Chao <ngokchao.ho@protonmail.com>
2021-02-26 00:33:26 +0800

    Add pointer to tail in queue_t and implement q_new
    - add pointer to tail so that q_insert_tail can be O(1)
    - implement q_new to initialise queue's head and tail to null and size 0

--- queue.c
+++ queue.c
@@ -12,8 +12,11 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
-    q->head = NULL;
+    if (q != NULL) {
+        q->head = NULL;
+        q->size = 0;
+        q->tail = NULL;
+    }
     return q;
 }
 
